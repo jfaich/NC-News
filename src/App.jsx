fffeae7723c21331ef51e0d04408f66878db564d
jfaich <45927468+jfaich@users.noreply.github.com>
2025-02-19 20:54:24 +0000

    view a list of all articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Articles from "./Articles";
+
+import { Routes, Route } from "react-router";
+import Home from "./Home";
+import Article from "./Article";
+import Header from "./Header";
+import NavBar from "./NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <h1>Home page</h1>
-      </div>
-      <Articles></Articles>
-    </>
+    <div>
+      <Header></Header>
+      <NavBar className="nav-bar"></NavBar>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/article/:article_id" element={<Article />}></Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/Articles.jsx
+++ src/Articles.jsx
@@ -2,22 +2,37 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import ArticleCard from "./ArticleCard";
 const BaseURL = "https://news-api-9kug.onrender.com";
+import { Link } from "react-router";
 
 function Articles() {
   const [articlesList, setArticlesList] = useState([]);
+  const [isLoading, setIsLoading] = useState([false]);
 
   useEffect(() => {
+    setIsLoading(true);
     axios.get(`${BaseURL}/api/articles`).then((response) => {
       setArticlesList(response.data);
+      setIsLoading(false);
     });
   }, []);
+
+  if (isLoading) {
+    return <h2>Loading articles...</h2>;
+  }
   return (
     <section>
       <div>
         <p>Showing All Articles ({articlesList.length} available)</p>
-        <ol>
+        <ol className="articles-list">
           {articlesList.map((article) => {
-            return <ArticleCard article={article}></ArticleCard>;
+            return (
+              <Link to="/article/">
+                <ArticleCard
+                  className="article-card"
+                  article={{ article }}
+                ></ArticleCard>
+              </Link>
+            );
           })}
         </ol>
       </div>
